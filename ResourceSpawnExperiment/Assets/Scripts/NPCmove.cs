31024b5c45640a1deb621bd4a43b6ada975185fa
Max Wong <wong.max.c@gmail.com>
2018-09-27 22:25:09 -0400

    Mastered NavMesh features in Unity
    With this update, I have been able to familliarize myself with NavMeshes

--- ResourceSpawnExperiment/Assets/Scripts/NPCmove.cs
+++ ResourceSpawnExperiment/Assets/Scripts/NPCmove.cs
@@ -1,11 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngin.AI;
+using UnityEngine.AI;
 
 public class NPCmove : MonoBehaviour {
 
-    [SerializedField]
+ /*   [SerializedField]
     Transform _destination;
 
     NavMeshAgent _navMeshAgent;
@@ -15,7 +15,7 @@ public class NPCmove : MonoBehaviour {
     {
         _navMeshAgent = this.GetComponent<NavMeshAgent>();
 
-        if(_navMeshAgent == num)
+        if(_navMeshAgent == null)
         {
             Debug.LogError("The nav mesh agent component is not attacked to " + gameObject.name);
         }
@@ -40,4 +40,8 @@ public class NPCmove : MonoBehaviour {
     {
 		
 	}
+*/
+
+
+
 }
