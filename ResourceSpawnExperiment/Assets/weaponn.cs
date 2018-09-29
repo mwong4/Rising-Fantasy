75eebd78dabda83c546dce13b112d02e7bd84538
LilMerp <43502772+LilMerp@users.noreply.github.com>
2018-09-29 16:55:59 -0400

    turret_faceenemyandinstantiate

---
+++ ResourceSpawnExperiment/Assets/weaponn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponn : MonoBehaviour {
+
+	public Vector3 lookPos;
+
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			transform.LookAt (target);
+	}
+}
