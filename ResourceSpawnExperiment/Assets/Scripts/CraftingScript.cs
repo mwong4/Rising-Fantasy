301a115464aa0179eb47c5abb397d0143d4b1d17
Max Wong <wong.max.c@gmail.com>
2018-09-30 21:40:54 -0400

    created the base for a crafting system

---
+++ ResourceSpawnExperiment/Assets/Scripts/CraftingScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftingScript : MonoBehaviour {
+
+    //public GameObject Object(#);
+
+    public GameObject Object1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+
+        if(col.gameObject.tag == "")
+        {
+            Instantiate(Object1, this.transform.position, Quaternion.identity);
+            Destroy(col.GameObject);
+            Destroy(this.GameObject);
+        }
+    }
+}
