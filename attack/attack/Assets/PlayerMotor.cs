1f87d450325043c99c51c95457ff26206b4f3572
Jedikoops <43502813+Jedikoops@users.noreply.github.com>
2018-10-06 17:21:39 -0400

    working on the sword attack

---
+++ attack/attack/Assets/PlayerMotor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour 
+{
+	private CharacterController controller;
+
+	private float verticalVelocity;
+	private float gravity = 14.0f;
+	private float jumpForce = 10.0f;
+
+	private void Start()
+	{
+		controller = GetComponent<CharacterController> ();
+	}
+
+	private void Update()
+	{
+		if(controller.isGrounded)
+		{
+			verticalVelocity = -gravity * Time.deltaTime;
+			if (Input.GetKeyDown (KeyCode.Space)) 
+			{
+				verticalVelocity = jumpForce;
+			}
+		}
+		else
+		{
+			verticalVelocity-= gravity * Time.deltaTime;
+		}
+	}
+}
+
---
+++ attack/attack/Assets/Swordattack.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swordattack : MonoBehaviour {
+
+	private Animator animator;
+	WeaponSwitching wp;
+
+	// Use this for initialization
+	void Start () 
+	{
+		wp = GetComponent<WeaponSwitching> ();
+		animator = GetComponent<Animator> ();
+	}
+	public void PerformAttack()
+	{
+		animator.SetTrigger("Base_Attack");
+	}
+	// Update is called once per frame
+	void Update () 
+
+		{
+			if(Input.GetMouseButtonDown(0))
+			{
+				
+				if(wp.selectedWeapon == 1)
+	
+				{
+				//The place where you are supposed to put your code
+				}
+			}
+			
+	}
+}
+				//17:21
