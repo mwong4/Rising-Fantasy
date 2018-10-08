eb6a62fde73ddbe47c85ac1963caab2773d8be5c
Max Wong <wong.max.c@gmail.com>
2018-10-07 23:01:25 -0400

    Because of my stupid mistakes, I manually merged shooting and basic enemy with pathfinding and basic XP system

---
+++ ResourceSpawnExperiment/Assets/Scripts/shooty.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooty : MonoBehaviour {
+
+    public float speed;
+
+	Movement mov;
+
+	// Use this for initialization
+	void Start () {
+		mov = GameObject.FindGameObjectWithTag ("Player").GetComponent<Movement> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.MoveTowards (transform.position, mov.endPoint, speed * Time.deltaTime);
+		Destroy (gameObject, 1f);
+	
+	}
+	void OnCollisionEnter(Collision collision)
+	{
+
+			Destroy (gameObject, 0.3f);
+	} 
+}
+
