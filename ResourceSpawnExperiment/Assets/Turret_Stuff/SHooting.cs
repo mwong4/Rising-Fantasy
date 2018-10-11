448cd682db5a6a1588c291d0f10bddd15c605ade
LilMerp <43502772+LilMerp@users.noreply.github.com>
2018-10-11 15:38:12 -0400

    Turret_FireRate8

---
+++ ResourceSpawnExperiment/Assets/Turret_Stuff/SHooting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SHooting : MonoBehaviour {
+
+	public GameObject projectile;
+
+	public Transform shotPoint;
+
+	public float fireRate = 10f;
+	private float nextTimeToFire = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time >= nextTimeToFire) {
+			Instantiate (projectile, shotPoint.position, shotPoint.rotation);
+			nextTimeToFire = Time.time + 1.5f / fireRate;
+
+		}
+	}
+}
---
+++ ResourceSpawnExperiment/Assets/Turret_Stuff/shootytt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootytt : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//this.transform.position = new Vector3 (direction.x, direction.y + speed, direction.z);
+
+		transform.Translate (0, -speed * Time.deltaTime, 0);
+
+	}
+
+	void OnCollisionEnter(){
+		Destroy (gameObject, 0.1f);
+	}
+
+}
