be3f811066598d36fb280545bbe8f597918bc221
Max Wong <wong.max.c@gmail.com>
2018-10-19 23:47:09 -0400

    upgraded the time system

--- ResourceSpawnExperiment/Assets/Scripts/TimeSystem.cs
+++ ResourceSpawnExperiment/Assets/Scripts/TimeSystem.cs
@@ -6,6 +6,7 @@ public class TimeSystem : MonoBehaviour {
 
     public float currentTime;
     public float TimeChange;
+    public float PhaseIncreasePercent;
 
     //Phases
     public float BuildingPhase;
@@ -15,6 +16,8 @@ public class TimeSystem : MonoBehaviour {
     public float EndCycle;
 
     public float CurrentPhase;
+
+    int EnemyCount;
 
 	// Use this for initialization
 	void Start () {
@@ -28,29 +31,35 @@ public class TimeSystem : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        EnemyCount = GameObject.FindGameObjectsWithTag("RawWoodResource").Length;
+
         currentTime += TimeChange;
 
 
         if(BuildingPhase < currentTime  && CurrentPhase == 1){
             CurrentPhase = 2f;
+
             //do something
         }
 
         if (CollectingPhase < currentTime && CurrentPhase == 2)
         {
             CurrentPhase = 3f;
+
             //do something
         }
 
         if (SpawningPhase < currentTime && CurrentPhase == 3)
         {
             CurrentPhase = 4f;
+
             //do something
         }
 
-        if (BattlingPhase < currentTime && CurrentPhase == 4)
+        if (BattlingPhase < currentTime && CurrentPhase == 4 || EnemyCount < 0 && CurrentPhase == 4)
         {
             CurrentPhase = 5f;
+
             //do something
         }
 
@@ -58,14 +67,25 @@ public class TimeSystem : MonoBehaviour {
         {
             currentTime = 0f;
             CurrentPhase = 1f;
+
             //do something
         }
 
         if (EndCycle < currentTime && CurrentPhase == 4)
         {
+            CurrentPhase = 5f;
+            //do something
+        }
+        if (currentTime < (EndCycle + 1) && CurrentPhase == 5){
+            
             currentTime = 0f;
             CurrentPhase = 1f;
-            //do something
+
+            BuildingPhase = BuildingPhase + PhaseIncreasePercent;
+            CollectingPhase = CollectingPhase + PhaseIncreasePercent;
+            SpawningPhase = SpawningPhase + PhaseIncreasePercent;
+            BattlingPhase = BattlingPhase + PhaseIncreasePercent;
+            EndCycle = EndCycle + PhaseIncreasePercent;
         }
 		
 	}
