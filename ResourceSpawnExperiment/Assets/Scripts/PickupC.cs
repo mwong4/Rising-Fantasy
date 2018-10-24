f8730bcd09d8dfe147cb5ea332c3450f60d6ab02
Max Wong <wong.max.c@gmail.com>
2018-10-23 21:54:57 -0400

    I had to manually merge the pickup into the resource scenes

---
+++ ResourceSpawnExperiment/Assets/Scripts/PickupC.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupC : MonoBehaviour {
+	public Transform onhand;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown()
+	{
+		GetComponent<Rigidbody>().useGravity = false;
+		this.transform.position = onhand.position;
+		this.transform.parent = GameObject.Find("ThirdPersonController").transform;
+		this.transform.parent = GameObject.Find("EthanSkeleton").transform;
+	}
+
+	void OnMouseUp()
+	{
+		transform.parent = null;
+		GetComponent<Rigidbody>().useGravity = true;
+	}
+}
