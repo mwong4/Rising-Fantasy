2edad806a369a39a35a18f7daaf35a36ff19fc80
LilMerp <43502772+LilMerp@users.noreply.github.com>
2018-10-29 19:24:36 -0400

    major bug fixes

--- ResourceSpawnExperiment/Assets/Scripts/EnemyScript.cs
+++ ResourceSpawnExperiment/Assets/Scripts/EnemyScript.cs
@@ -7,65 +7,54 @@ public class EnemyScript : MonoBehaviour {
 
 	public float speed;
 	public float health;
-	public float Curhealth;
-
+	private float curHealth;
     public float BulletDamage;
 
-	public Transform target;
-
-	public ParticleSystem enemyDeath;
+	public GameObject enemyDeath;
 	public GameObject turnOnBar;
 	public Image image;
 
-    public GameObject myPlayer;
+	GameObject myPlayer;
+
 
 	// Use this for initialization
 	void Start () {
-		enemyDeath.Stop ();
-		Curhealth = health;
-
-		target = GameObject.FindGameObjectWithTag ("Tower").GetComponent<Transform> ();
+		curHealth = health;
 		turnOnBar = GameObject.Find ("HealthBar_Gameobject");
 		turnOnBar.SetActive (false);
 
         myPlayer = GameObject.FindGameObjectWithTag("Player");// Get XP Script and xpPoints value
         PlayerXP playerScript = myPlayer.GetComponent<PlayerXP>();
 
-        BulletDamage = 1f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = Vector3.MoveTowards (transform.position, target.position, speed * Time.deltaTime);	
 
 		if (health <= 0) {
 			CommitDie ();
 		}
-
-        Debug.Log("BulletDamage is :" + BulletDamage);
-
-
-
 	}
 
 	void OnCollisionEnter(Collision collision)
 	{
 		if (collision.gameObject.tag == "Projectile") {
-			
-			image.fillAmount = image.fillAmount - BulletDamage / Curhealth;
+
 			health = health - BulletDamage;
+			
+			image.fillAmount = image.fillAmount - BulletDamage / 10;
+
 			turnOnBar.SetActive (true);
 		}
 	} 
 
 	void CommitDie (){
-        myPlayer.GetComponent<PlayerXP>().CurrentXP += 10f;
+        //myPlayer.GetComponent<PlayerXP>().CurrentXP += 10f;
         
         //Justin, Right here, you should instantiate the particle effect instead -Max
-        enemyDeath.Play();
 
 		Destroy (this.gameObject);
-		enemyDeath.transform.position = transform.position;
+		Instantiate (enemyDeath, transform.position, Quaternion.identity);
 
 	}
 }
