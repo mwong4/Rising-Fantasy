7c31578d8ad2993b95c0af884a22f3f384a1a93e
Max Wong <wong.max.c@gmail.com>
2018-11-02 22:56:29 -0400

    Added in the movement scripts. just need to test and update them.

---
+++ ResourceSpawnExperiment/Assets/Scripts/NewMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewMovement : MonoBehaviour {
+
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		moveSpeed = 7f;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate (moveSpeed * Input.GetAxis ("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis ("vertical") * Time.deltaTime);
+		
+	}
+}
---
+++ ResourceSpawnExperiment/Assets/Scripts/NewbJump.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewbJump : MonoBehaviour {
+
+	public float fallMultiplier = 2.5f;
+	public float lowjumpMultiplier = 2f;
+
+	Rigidbody rb;
+
+	void Awake(){
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rb.velocity.y < 0) {
+			rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+		} else if (rb.velocity.y > 0 && !Input.GetButton ("Jump")) {
+			rb.velocity += Vector3.up * Physics.gravity.y * (lowjumpMultiplier - 1) * Time.deltaTime;
+		}
+		
+	}
+}
