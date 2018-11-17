12c85f68bff721f524b97f73fae05d5b1e648765
FArterbox <43502406+FArterbox@users.noreply.github.com>
2018-11-17 17:15:56 -0500

    added stuff

---
+++ implement/New Unity Project (2)/Assets/movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	public bool moving;
+
+
+
+		void Start ()
+	{
+
+		moving = true;
+		}
+
+		void Update ()
+		{
+
+		if (moving == true) {
+				var x = Input.GetAxis ("Horizontal") * Time.deltaTime * 150.0f;
+				var z = Input.GetAxis ("Vertical") * Time.deltaTime * 5.0f;
+
+				transform.Rotate (0, x, 0);
+				transform.Translate (0, 0, z);
+			}
+		}
+	
+	}
