6e70558ca706311dfa04a3e719cc5de519da9f1d
Jedikoops <43502813+Jedikoops@users.noreply.github.com>
2018-11-21 01:44:18 -0500

    more done

--- ResourceSpawnExperiment/Assets/Scripts/CameraFollow.cs
+++ ResourceSpawnExperiment/Assets/Scripts/CameraFollow.cs
@@ -4,40 +4,10 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
-	public Transform PlayerTransform;
+	public Transform target;
 
-	private Vector3 _cameraOffset;
+	public float smoothSpeed = 0.125f;
+	public Vector3 offset;
 
-	[Range(0.01f, 1.0f)]
-	public float SmoothFactor = 0.5f;
 
-	public bool LookAtPlayer = false;
-
-	public bool RotateAroundPlayer = true;
-
-	public float RotationsSpeed = 5.0f;
-
-	// Use this for initialization
-	void Start () {
-		_cameraOffset = transform.position - PlayerTransform.position;
-	}
-
-	//Update is called once per frame
-	void LateUpdate () {
-
-		if (RotateAroundPlayer) 
-		{
-			Quaternion camTurnAngle =
-				Quaternion.AngleAxis(Input.GetAxis("Mouse X") * RotationsSpeed, Vector3.up);
-
-			_cameraOffset = camTurnAngle * _cameraOffset;
-		}
-
-		Vector3 newPos = PlayerTransform.position + _cameraOffset;
-
-		transform.position = Vector3.Slerp (transform.position, newPos, SmoothFactor);
-
-		if (LookAtPlayer || RotateAroundPlayer)
-			transform.LookAt (PlayerTransform);
-	}
 }
---
+++ ResourceSpawnExperiment/Assets/Scripts/CameraRotatoe.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotatoe : MonoBehaviour {
+
+	public GameObject targetObject;
+	private float targetAngle = 0;
+	const float rotationAmount = 1.5f;
+	public float rDistance = 1.0f;
+	public float rSpeed = 1.0f;
+	//new rules
+	public Transform target;
+	//public float smoothSpeed = 0.125f;		just extra
+	public Vector3 offset;
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		// Trigger functions if Rotate is requested
+		if (Input.GetKeyDown(KeyCode.Q)) {
+			targetAngle -= 90.0f;
+		} else if (Input.GetKeyDown(KeyCode.E)) {
+			targetAngle += 90.0f;
+		}
+
+		if(targetAngle !=0)
+		{
+			Rotate();
+		}
+	}
+
+	protected void Rotate()
+	{
+
+		float step = rSpeed * Time.deltaTime;
+		float orbitCircumfrance = 2F * rDistance * Mathf.PI;
+		float distanceDegrees = (rSpeed / orbitCircumfrance) * 360;
+		float distanceRadians = (rSpeed / orbitCircumfrance) * 2 * Mathf.PI;
+
+		if (targetAngle>0)
+		{
+			transform.RotateAround(targetObject.transform.position, Vector3.up, -rotationAmount);
+			targetAngle -= rotationAmount;
+		}
+		else if(targetAngle <0)
+		{
+			transform.RotateAround(targetObject.transform.position, Vector3.up, rotationAmount);
+			targetAngle += rotationAmount;
+		}
+
+	}
+}
+
+
+//link...
+//https://answers.unity.com/questions/655896/rotating-a-camera-90-degrees-around-an-object.html
