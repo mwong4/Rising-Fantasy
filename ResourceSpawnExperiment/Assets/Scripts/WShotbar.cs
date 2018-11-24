7f7d1b911ecfb8e205117370d1c082d28e2f8c68
jasperNie <31317861+jasperNie@users.noreply.github.com>
2018-11-24 17:00:03 -0500

    added in a hot bar

---
+++ ResourceSpawnExperiment/Assets/Scripts/WShotbar.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+
+public class WShotbar : MonoBehaviour {
+
+	public int selectedWeapon = 0;
+	public GameObject[] selections;
+	// Use this for initialization
+	void Start () {
+		//selections = GameObject.FindGameObjectsWithTag ("selection");
+		SelectWeapon(0);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		int previousSelectedWeapon = selectedWeapon;
+
+		if (Input.GetAxis ("Mouse ScrollWheel") > 0f) 
+		{
+			if (selectedWeapon >= selections.Length-1)
+				selectedWeapon = 0;
+			else
+				selectedWeapon++;
+				
+			SelectWeapon(selectedWeapon);
+		}
+		if (Input.GetAxis ("Mouse ScrollWheel") < 0f) 
+		{
+			if (selectedWeapon <= 0f)
+				selectedWeapon = selections.Length-1;
+			else
+				selectedWeapon--;
+				
+			SelectWeapon(selectedWeapon);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) 
+		{
+			selectedWeapon = 0;
+			SelectWeapon (0);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha2) && transform.childCount >= 2) 
+		{
+			selectedWeapon = 1;
+			SelectWeapon (1);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha3) && transform.childCount >= 3) 
+		{
+			selectedWeapon = 2;
+			SelectWeapon (2);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha4) && transform.childCount >= 4) 
+		{
+			selectedWeapon = 3;
+			SelectWeapon (3);
+		}
+
+
+
+		//if (previousSelectedWeapon != selectedWeapon) 
+		//{
+		//	SelectWeapon ();
+		//}
+	}
+
+	void SelectWeapon (int selection)
+	{
+		for(int i = 0; i<selections.Length;i++)
+		{
+			if (i == selection)
+				selections[i].gameObject.SetActive (true);
+			else
+				selections[i].gameObject.SetActive (false);
+		}
+
+	}
+}
