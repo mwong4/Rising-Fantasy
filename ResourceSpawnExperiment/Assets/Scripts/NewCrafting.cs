cee293a0f40fda0759952d4d1888549f0daf4134
MW <43390816+mwong4@users.noreply.github.com>
2018-11-29 22:55:27 -0500

    Crafting semi-finished. Still has a referencing bug and the spawning of the turret must be made

---
+++ ResourceSpawnExperiment/Assets/Scripts/NewCrafting.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewCrafting : MonoBehaviour {
+
+	public bool touchingRock;
+	public bool touchingWood;
+
+	public bool destroyMaterials;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (touchingWood == true && touchingRock == true) {
+			
+			//Spawn turret
+
+			destroyMaterials = true;
+		} else {
+			destroyMaterials = false;
+		}
+		
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if ( collision.gameObject.tag == "RawRockResource" || collision.gameObject.tag == "RawRockResourceTwo" || collision.gameObject.tag == "RawRockResourceThree" ) {
+			touchingRock = true;
+
+		}
+		if ( collision.gameObject.tag != "RawRockResource" && collision.gameObject.tag != "RawRockResourceTwo" && collision.gameObject.tag != "RawRockResourceThree" ) {
+			touchingRock = false;
+
+		}
+		if ( collision.gameObject.tag == "RefinedWoodResource"  ) {
+			touchingWood = true;
+
+		}
+		if ( collision.gameObject.tag != "RefinedWoodResource"  ) {
+			touchingWood = false;
+
+		}
+		if (destroyMaterials == true) {
+			Destroy (collider.gameObject);
+		}
+	} 
+}
--- ResourceSpawnExperiment/Assets/Scripts/TurretPlacement.cs
+++ ResourceSpawnExperiment/Assets/Scripts/TurretPlacement.cs
@@ -4,63 +4,78 @@ using UnityEngine;
 
 public class TurretPlacement : MonoBehaviour {
 	
-	public float distanceFromPlayer; //variable to hold the distance between player and platform
-	public float lockRadius; // min distance to lock position0
+	public float distanceFromResourceOne; //variable to hold the distance between Resource and platform
+	public float distanceFromResourceTwo; //variable to hold the distance between Resource and platform
+	public float distanceFromResourceThree; //variable to hold the distance between Resource and platform
+	public float distanceFromResourceFour; //variable to hold the distance between Resource and platform
 
-	public float currentTime; //variable holding artificial time
-	public float delay; //delay that the position is locked
+	public float lockRadius; // min distance to lock position
 
-	//variable to hold the players transform
-	public Transform myPlayerTransform;
+	//public float currentTime; //variable holding artificial time
+	//public float delay; //delay that the position is locked
 
-	public bool lockingPosition;
-	public bool delayActive;
+	//variable to hold the resources transform
+	public Transform myResourceTransform;
+	public Transform mySecondResourceTransform;
+	public Transform myThirdResourceTransform;
+	public Transform myFourthResourceTransform;
 
-	public WeaponSwitching myWeaponSwitching;
+	//public bool lockingPosition;
+	//public bool delayActive;
+
+	public TimeSystem myTimeSystem;
 
 
 	// Use this for initialization
 	void Start () {
 
-		myWeaponSwitching = GameObject.FindWithTag ("SelectWeapon").GetComponent<WeaponSwitching> ();
+		myTimeSystem = GameObject.FindWithTag ("Phases").GetComponent<TimeSystem> ();
 
 		//referencing and setting "myPlayer" as the player's transform
-		myPlayerTransform = GameObject.FindWithTag ("Player").transform;
+		myResourceTransform = GameObject.FindWithTag ("RefinedWoodResource").transform;
+		mySecondResourceTransform = GameObject.FindWithTag ("RawRockResource").transform;
+		myThirdResourceTransform = GameObject.FindWithTag ("RawRockResourceTwo").transform;
+		myFourthResourceTransform = GameObject.FindWithTag ("RawRockResourceThree").transform;
 
-		currentTime = 0;
 
-		lockingPosition = false;
-		delayActive = false;
+		//currentTime = 0;
+
+		//lockingPosition = false;
+		//delayActive = false;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		//setting "distanceFromPlayer" as the distance between the player and platform transofrm
-		distanceFromPlayer = Vector3.Distance (myPlayerTransform.position, transform.position);
+		//Asigning the distance
+		distanceFromResourceOne = Vector3.Distance (myResourceTransform.position, transform.position);
+		distanceFromResourceTwo = Vector3.Distance (mySecondResourceTransform.position, transform.position);
+		distanceFromResourceThree = Vector3.Distance (myThirdResourceTransform.position, transform.position);
+		distanceFromResourceFour = Vector3.Distance (myFourthResourceTransform.position, transform.position);
 
-		/*Checking to see if the distance is close enough to lock*/
-		if (distanceFromPlayer <= lockRadius && lockingPosition == false && myWeaponSwitching.selectedWeapon == 0) {
-
-				lockingPosition = true;//Lock Player onto the platform's transform
-				delayActive = true;
-				myPlayerTransform.transform.position = new Vector3 (transform.position.x, transform.position.y + 1, transform.position.z);
+		if (distanceFromResourceOne >= lockRadius && myTimeSystem.CurrentPhase <= 3) {
+			
+			myResourceTransform.transform.position = new Vector3 (transform.position.x, transform.position.y + 1, transform.position.z);
 
 		}
 
-		if (delayActive == true) {
-			currentTime += 0.1f;
-			myPlayerTransform.transform.position = new Vector3 (transform.position.x, transform.position.y + 1, transform.position.z);
-		}
-		if (currentTime >= delay && delayActive == true) {
-			delayActive = false;
-			currentTime = 0;
+		if (distanceFromResourceTwo >= lockRadius && myTimeSystem.CurrentPhase <= 3) {
+
+			mySecondResourceTransform.transform.position = new Vector3 (transform.position.x, transform.position.y + 1, transform.position.z);
+
 		}
 
+		if (distanceFromResourceThree >= lockRadius && myTimeSystem.CurrentPhase <= 3) {
 
-		if (distanceFromPlayer >= lockRadius && lockingPosition == true) {
-			lockingPosition = false;
+			myThirdResourceTransform.transform.position = new Vector3 (transform.position.x, transform.position.y + 1, transform.position.z);
+
+		}
+
+		if (distanceFromResourceFour >= lockRadius && myTimeSystem.CurrentPhase <= 3) {
+
+			myFourthResourceTransform.transform.position = new Vector3 (transform.position.x, transform.position.y + 1, transform.position.z);
+
 		}
 		
 	}
