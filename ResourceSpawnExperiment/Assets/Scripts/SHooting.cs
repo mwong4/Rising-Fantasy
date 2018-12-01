763b2a260326f52f7ca16132a5fc1f761766b6f2
MW <43390816+mwong4@users.noreply.github.com>
2018-12-01 16:16:01 -0500

    Trying to fix the turret

--- ResourceSpawnExperiment/Assets/Scripts/SHooting.cs
+++ ResourceSpawnExperiment/Assets/Scripts/SHooting.cs
@@ -13,13 +13,47 @@ public class SHooting : MonoBehaviour {
 	public float currentTime;
 	public float delay;
 
+	Transform lookAt;
+
+	public List<GameObject> enemiesInRange = new List<GameObject> ();
+
+	public bool enemyEntered= false;
+
 	// Use this for initialization
 	void Start () {
+		lookAt = GameObject.FindGameObjectWithTag("Crosshair").GetComponent<Transform>();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		//transform.LookAt (lookAt);
+
+
+
+		if (enemiesInRange.Count == 0) {
+
+		} else {
+			GameObject closest = null;
+			float distance = Mathf.Infinity;
+			Vector3 position = transform.position;
+
+			foreach (GameObject x in enemiesInRange) {
+
+				Vector3 diff = x.transform.position - position;
+				float curDistance = diff.sqrMagnitude;
+				if (curDistance < distance)
+				{
+					closest = x;
+					distance = curDistance;
+				}
+			}
+			//return closest;
+
+			this.transform.LookAt(closest.transform.position);
+
+		}
 
 		currentTime += 1;
 
@@ -27,6 +61,14 @@ public class SHooting : MonoBehaviour {
 		if (currentTime >= delay) {
 			Instantiate (projectile, shotPoint.position, shotPoint.rotation);
 			currentTime = 0;
+		}
+	}
+
+	void OnTriggerEnter(Collider collision)
+	{
+		if (collision.gameObject.tag == "Enemy") {
+			enemyEntered = true;
+			enemiesInRange.Add (collision.gameObject);
 		}
 	}
 }
